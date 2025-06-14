b23763944265bccd815ea4141123f8383f11955e
jessiica.bender@gmail.com <jessiica.bender@gmail.com>
2025-06-14 19:54:24 +0000

    rename

---
+++ using/staging/raw/stg_raw__sales.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id,
+        revenue,
+        quantity
+
+    from source
+
+)
+
+select * from renamed
+pdt_id to products_id
