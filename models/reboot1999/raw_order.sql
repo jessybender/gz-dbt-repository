0aa07525c0abfa1a6ca5334ecb53d19bc996894f
Jessica Bender <jessiica.bender@gmail.com>
2025-07-15 00:38:36 +0000

    excerise 1

--- models/reboot1999/raw_order.sql
+++
@@ -1,7 +0,0 @@
-select
-    customer_id,
-    min(ordered_at) as first_order_date,
-    max(ordered_at) as last_order_date,
-    count(order_id) as total_orders
-from {{ ref('orders') }}
-group by customer_id
